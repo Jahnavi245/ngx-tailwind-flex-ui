101d8d575b841f4bc03c06751ceeed2b553d426b
Jahnavi245 <jahnavi25102001@gmail>
2025-03-15 19:53:25 +0530

    add components

--- libs/ngx-tailwind-flex-ui/src/index.ts
+++ libs/ngx-tailwind-flex-ui/src/index.ts
@@ -2,3 +2,9 @@ export * from './lib/button/button.component';
 export * from './lib/checkbox/checkbox.component';
 export * from './lib/pin-input/pin-input.component';
 export * from './lib/rating/rating.component';
+
+
+
+
+
+
